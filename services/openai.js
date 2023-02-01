845912501d76de9702a663b17b818d94cf4ae1fe
FPChen <cmcreborn@gmail.com>
2023-02-01 11:45:12 +0800

    [MOD]延長timeout

--- services/openai.js
+++ services/openai.js
@@ -13,7 +13,7 @@ export const IMAGE_SIZE_1024 = '1024x1024';
 
 const instance = axios.create({
   baseURL: 'https://api.openai.com',
-  timeout: 19 * 1000,
+  timeout: 60 * 1000,
   headers: {
     'Accept-Encoding': 'gzip, deflate, compress',
   },
